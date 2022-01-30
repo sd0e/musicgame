fab543d01acc561478841d39cc2b3045f7e5ffbc
sebastiandoe5 <sebastiandoe5@gmail.com>
2022-01-30 17:27:38 +0000

    Fix hash question mark on gh-pages

--- src/components/layout/Layout.js
+++ src/components/layout/Layout.js
@@ -1,7 +1,16 @@
+import { useNavigate } from 'react-router-dom';
+
 import classes from "./Layout.module.css";
 import LeftMenu from "./LeftMenu";
 
 export default function Layout({ children }) {
+  let navigate = useNavigate();
+
+  if (window.location.href.includes('/?/')) {
+    const newPath = window.location.href.split('/?/')[1];
+    navigate(newPath);
+  }
+
   return (
     <table className={classes.tableOuter}>
       <tr>
