f67a60e279014b50331a3865e9dea4f727b6a703
sebastiandoe5 <sebastiandoe5@gmail.com>
2022-01-30 14:39:30 +0000

    Remove homepage from package.json

--- src/components/layout/LeftMenu.js
+++ src/components/layout/LeftMenu.js
@@ -55,7 +55,7 @@ export default function LeftMenu() {
             <span className={classes.gameTitleText}>Music Game</span>
           </span>
           <Button variant="contained">
-            <ButtonText Icon={Home} Name="Test Push Again" />
+            <ButtonText Icon={Home} Name="Home" />
           </Button>
         </div>
         <Button onClick={changeTheme}>
